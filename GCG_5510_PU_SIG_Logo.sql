cbf090594e88236e3608bcd6924810aa7ecc4ea1
Service Web <sw@gssmail.com>
2024-03-14 11:27:21 -0500

    Commit changes in C:\Websites\Git\emarlow\5510\GCG_5510_PU_SIG_Logo.sql

---
+++ GCG_5510_PU_SIG_Logo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "GCG_5510_PU_SIG_Logo"(
+ "Buyer" CHAR(3),
+ "Buyer_Name" CHAR(30),
+ "Signature" CHAR(50),
+ "Logo" CHAR(50) );
+ 
+ CREATE INDEX "IND0" ON "GCG_5510_PU_SIG_Logo"("Buyer");
